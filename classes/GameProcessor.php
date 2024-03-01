21127697d95ea6e6f76e81055692ed0b9f12e789
h-iranshahi <h.iranshahi67@gmail.com>
2024-03-01 17:44:32 +0330

    Implement the logic of race using cli tools

--- classes/GameProcessor.php
+++ classes/GameProcessor.php
@@ -30,7 +30,56 @@ class GameProcessor
 
     public function start()
     {
-        return 'hello';
+        \cli\out("%yWelcome to the Racing Game!\n%n", 'green');
+        \cli\out("Select your vehicle for player 1\n");
+
+        foreach ($this->vehicles as $index => $vehicle) {
+            \cli\out("{$index}. {$vehicle->getName()} (Max Speed: {$vehicle->getMaxSpeed()} km/h)\n");
+        }
+
+        $selected = \cli\prompt('Enter the number of vehicle for player 1');
+
+        if (!isset($this->vehicles[$selected])) {
+            \cli\err("Invalid selection. Please try again.\n");
+            return;
+        }
+
+        $vehicle1 = $this->vehicles[$selected];
+
+        \cli\out('%bYou selected: ' . $vehicle1->getName() . "\n%n");
+
+
+        \cli\out("Select your vehicle for player 2\n");
+
+        foreach ($this->vehicles as $index => $vehicle) {
+            \cli\out("{$index}. {$vehicle->getName()} (Max Speed: {$vehicle->getMaxSpeed()} km/h)\n");
+        }
+
+        $selected = \cli\prompt('Enter the number of vehicle for player 2');
+
+        if (!isset($this->vehicles[$selected])) {
+            \cli\err("Invalid selection. Please try again.\n");
+            return;
+        }
+
+        $vehicle2 = $this->vehicles[$selected];
+
+        \cli\out('%bYou selected: ' . $vehicle2->getName() . "\n%n");
+
+        $raceTime1 = $this->calculateRaceTime($vehicle1->getMaxSpeed());
+        $raceTime2 = $this->calculateRaceTime($vehicle2->getMaxSpeed());
+
+        \cli\out('Calculating race time...'."\n");
+        \cli\out("Player 1 race time is: {$raceTime1} seconds"."\n");
+        \cli\out("Player 2 race time is: {$raceTime2} seconds"."\n");
+        \cli\out("%gPlayer ".($raceTime1 > $raceTime2 ? '2' : '1')." wins %n");
+
+    }
+
+    private function calculateRaceTime($speed)
+    {
+        $path = 5 ; // km
+        return round(($path / $speed)*60*60);
     }
 
 }
